fe92a7a1cc3968abf83563839c98e17d8d62ff85
Hasan_Lenovo <hassan.9th@gmail.com>
2026-07-18 23:33:32 +0330

    Api init

---
+++ backend/src/app.ts
@@ -0,0 +1,8 @@
+import express from "express"
+const app=express();
+
+const PORT=3001;
+
+app.get("/",(req,res)=>{res.json({message:"This is a message from server"});
+});
+app.listen(PORT,()=>{console.log(`Server is running on http://localhost:${PORT}`)});
