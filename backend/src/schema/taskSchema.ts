ddffe69439e60baccec628a428b28bfd7fbb771a
hasan9th <30544201+hasan9th@users.noreply.github.com>
2026-08-18 13:26:05 +0330

    add update and delete method

--- backend/src/schema/taskSchema.ts
+++ backend/src/schema/taskSchema.ts
@@ -3,3 +3,9 @@ import { z } from "zod";
 export const createTaskSchema = z.object({
   title: z.string().min(1),
 });
+export const updateTaskSchema=z.object(
+  {
+    title:z.string().min(3),
+    completed:z.boolean(),
+  }
+)
